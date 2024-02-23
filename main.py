8cb4bf04778e8ce0679b682f0204388719d5ac4f
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-02-23 10:46:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-dev_user_ids = []
+dev_user_ids = [842201757948968991]
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -21,15 +21,17 @@ async def on_command_error(ctx, error):
     error_channel = bot.get_channel(error_channel_id)
 
     if error_channel:
-        await error_channel.send(f"An error occurred: {type(error).__name__} - {error}")
+        embed = discord.Embed(title="Error", description=f"An error occurred: {type(error).__name__} - {error}", color=discord.Color.red())
+        await error_channel.send(embed=embed)
     else:
         print(f"An error occurred: {type(error).__name__} - {error}")
-
 
 for filename in os.listdir('./commands'):
     if filename.endswith('.py'):
         bot.load_extension(f'commands.{filename[:-3]}')
 
-token = os.getenv("Token")
+token = os.getenv("TOKEN")
 bot.run(token)
+
+
 keep_alive()
