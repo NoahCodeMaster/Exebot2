24cf7cbd4f27978a71270f423d1c3b74dfb35a22
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-02-23 11:10:30 +0200

    Update dev.py

--- commands/dev.py
+++ commands/dev.py
@@ -1,17 +1,11 @@
-from discord.ext import commands
-import os
-
-class DevCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(name='dev_restart', hidden=True)
-    async def dev_restart(self, ctx):
-        if ctx.author.id not in dev_user_ids:
-            return
-
-        await ctx.send("Restarting...")
-        await self.bot.close()
-
-def setup(bot):
-    bot.add_cog(DevCog(bot))
+module.exports = {
+  name: 'dev',
+  description: 'A developer command',
+  execute(message, args) {
+    if (message.author.id === 'your-discord-user-id') {
+      message.reply('You are a developer!');
+    } else {
+      message.reply('You are not a developer.');
+    }
+  },
+};
