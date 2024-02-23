7246b43c2d9efac2dbc0d9ddcd32bfa5f11af428
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-02-23 11:21:46 +0200

    Update dev.py

--- commands/dev.py
+++ commands/dev.py
@@ -2,7 +2,7 @@ module.exports = {
   name: 'dev',
   description: 'A developer command',
   execute(message, args) {
-    if (message.author.id === 'your-discord-user-id') {
+    if (message.author.id === '842201757948968991') {
       message.reply('You are a developer!');
     } else {
       message.reply('You are not a developer.');
